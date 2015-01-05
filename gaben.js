0afcf8505b353308907c6067a5bca768be35bb9f
Blade Nelson <powderblock@gmail.com>
2015-01-05 15:58:49 -0500

    Update images
    Update images to include more choices and options.

--- gaben.js
+++ gaben.js
@@ -28,12 +28,21 @@ function Randomize(images) {
   return Math.floor(Math.random() * images.length)
 }
 
-var myGabeN = [new GabeN("horizontal", "http://i.imgur.com/7MRsGme.jpg"), 
-new GabeN("horizontal", "http://i.imgur.com/ATeXA7w.jpg"), 
-new GabeN("horizontal", "http://i.imgur.com/m172FXu.jpg"), 
-new GabeN("vertical", "http://heygirl.io/img/GabeN-vert-1.jpg"), 
-new GabeN("vertical", "http://i.imgur.com/ATeXA7w.jpg"), 
-new GabeN("square", "http://i.imgur.com/7MRsGme.jpg")];
+var myGabeN = [
+new GabeN("horizontal", "http://i.imgur.com/uLMbKmj.jpg"), 
+new GabeN("horizontal", "http://i.imgur.com/5DXQ7tx.jpg"), 
+new GabeN("horizontal", "http://i.imgur.com/dldrXOf.jpg"), 
+new GabeN("horizontal", "http://i.imgur.com/8uH9TQb.jpg"), 
+new GabeN("horizontal", "http://i.imgur.com/7wU40jQ.jpg"), 
+new GabeN("horizontal", "http://i.imgur.com/ajxvdWo.jpg"), 
+new GabeN("vertical", "http://i.imgur.com/aRCPVmX.jpg"), 
+new GabeN("vertical", "http://i.imgur.com/FU2unZO.jpg"), 
+new GabeN("vertical", "http://i.imgur.com/3brJDRx.jpg"), 
+new GabeN("vertical", "http://i.imgur.com/j9KOnWm.jpg"), 
+new GabeN("square", "http://i.imgur.com/sWCcEwV.jpg"), 
+new GabeN("square", "http://i.imgur.com/ecbdpfL.jpg"), 
+new GabeN("square", "http://i.imgur.com/lDkMf5P.jpg"), 
+new GabeN("square", "http://i.imgur.com/qkcP48v.jpg")];
 
 function imageRatio(image) {
   var proportion = image.height / image.width;
